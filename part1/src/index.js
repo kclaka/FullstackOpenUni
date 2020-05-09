5215363dfff992304dca4c3ddc387b12afb1db2a
kclaka <kennyigbechi@gmail.com>
2020-05-09 00:48:34 -0700

    course information, step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -9,10 +9,20 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) =>{
   return(
     <div>
       <p>{props.part} {props.excercise}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      <Part part= {'Fundamentals of React'} excercise={10}/>
+      <Part part= {'Using props to pass data'} excercise={7}/>
+      <Part part= {'State of a component'} excercise={14}/>
     </div>
   )
 }
@@ -28,24 +38,18 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
+  //const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
+  //const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
+  //const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header name = {course}/>
       <p>
-        <Content part = {part1} excercise = {exercises1}/>
-      </p>
-      <p>
-      <Content part = {part2} excercise = {exercises2}/>
-      </p>
-      <p>
-      <Content part = {part3} excercise = {exercises3}/>
+        <Content/>
       </p>
       <p><Total count = {exercises1 + exercises2 + exercises3}/></p>
     </div>
