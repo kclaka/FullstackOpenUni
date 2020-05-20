a30775e58859ecfac523df446e89a162b18a08b4
kclaka <kennyigbechi@gmail.com>
2020-05-20 03:40:37 -0700

    course complete

--- part2/courseinformation/src/App.js
+++ part2/courseinformation/src/App.js
@@ -1,51 +1,73 @@
 import React from 'react'
 import Course from './components/Course'
-import Parts from './components/Parts'
+
+
 
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  let total = 0
-  course.parts.forEach(
-    part => total += part.exercises
-  )
+  
 
+  
   
 
   return (
     <div>
-      <Course name={course.name} />
-      <div>
-        {course.parts.map(
-          part =><Parts key={part.id} part={part.name} excercise={part.exercises}/>
-        )}
+      
+      
+      
+      {course.map( course =>
+        <Course key={course.id} course={course} />)}
+
       </div>
 
-      <p><b>total of {total} exercises</b></p>
-      
-    </div>
+       
+
   )
 }
 
--- part2/courseinformation/src/components/Course.js
+++ part2/courseinformation/src/components/Course.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 
-const Course = ({name})=>{
+const Heading = ({name}) => <h1>{name}</h1>
+
+const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+
+const Data = ({parts}) =>(
+    <>
+        {parts.map(part => (
+        <Part key={part.id} name={part.name} exercises={part.exercises}/>
+        ))}
+    </>
+)
+
+const Course = ({course}) =>{
+    const total = course.parts.reduce((prev, next) =>
+    ({exercises: prev.exercises + next.exercises}))
+
     return(
-        <h1>{name}</h1>
+        <>
+            <Heading course={course.name}/>
+            <Data parts= {course.parts}/>
+            <b>Total of {total.exercises} exercises</b>
+        </>
     )
 }
 
